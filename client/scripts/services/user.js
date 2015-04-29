99ace74276db4ac8ba4b43fdf4d26cc841c43621
tangpin <itangpin@163.com>
2015-04-29 09:52:24 +0800

    move status.service to service directory

--- client/scripts/services/user.js
+++ client/scripts/services/user.js
@@ -1,8 +1,8 @@
 /**
  * Created by pin on 4/28/15.
  */
-define(['./module'], function(serviceModule){
-    serviceModule.factory('service.user', [function(){
-
+define(['./module'], function (serviceModule) {
+    serviceModule.factory('service.user', ['$resource', function ($resource) {
+        return $resource()
     }])
 })
