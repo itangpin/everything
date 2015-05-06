ca0e2d3db7b1780f1e7065d06ba0a2806df2f624
tangpin <itangpin@163.com>
2015-05-06 15:21:29 +0800

    reformate

--- client/scripts/controllers/list.js
+++ client/scripts/controllers/list.js
@@ -13,60 +13,61 @@ define([
                          Everything,
                          APP) {
     controllerModule.controller('listController',
-        ['service.status','service.data',function (status,Data) {
+        ['service.status', 'service.data', function (status, Data) {
 
-        var defaultValue = [
-            {
-                content: '写作',
-                children: []
-            },
-            'Version 0.5.1',
-            {
-                content: "功能用法",
-                children: [
-                    '创建新的一行(Ctrl+Enter)',
-                    '向右缩进(Tab)',
-                    {
-                        content: '删除一行',
-                        children: [
-                            'delete键',
-                            '在空行上按退格键'
-                        ]
-                    },
-                    {
-                        content: "文本编辑器",
-                        package: ['editor']
-                    }
-                ],
-                expand: false
+
+            var defaultValue = [
+                {
+                    content: '写作',
+                    children: []
+                },
+                'Version 0.5.1',
+                {
+                    content: "功能用法",
+                    children: [
+                        '创建新的一行(Ctrl+Enter)',
+                        '向右缩进(Tab)',
+                        {
+                            content: '删除一行',
+                            children: [
+                                'delete键',
+                                '在空行上按退格键'
+                            ]
+                        },
+                        {
+                            content: "文本编辑器",
+                            package: ['editor']
+                        }
+                    ],
+                    expand: false
+                }
+            ]
+
+            var initList = function (data) {
+                var container = document.querySelector(".everything")
+                var everything = new Everything(data, APP, {
+                    container: container,
+                    theme: 'light'
+                });
             }
-        ]
 
-        var initList = function (data) {
-            var container = document.querySelector(".everything")
-            var everything = new Everything(data, APP, {
-                container: container,
-                theme: 'light'
-            });
-        }
-
-        status.updateSignInStatus(function (status, err) {
-            if (err) return
-            if (status == 'true') {
-                Data.getListData().success(function (data) {
-                    Data.updateLocal(data)
-                    initList(data)
-                })
-            } else if (status == 'false') {
-                Data.getListDataFromLocal(function (data) {
-                    if (data == null) {
-                        initList(defaultValue)
-                    } else{
+            status.updateSignInStatus(function (status, err) {
+                if (err) return
+                if (status == 'true') {
+                    Data.getListData().success(function (data) {
+                        Data.updateLocal(data)
                         initList(data)
-                    }
-                })
-            }
-        })
+                    })
+                } else if (status == 'false') {
+                    Data.getListDataFromLocal(function (data) {
+                        if (data == null) {
+                            initList(defaultValue)
+                        } else {
+                            initList(data)
+                        }
+                    })
+                }
+            })
 
-    }])
+        }])
 })
